6733a234eff9afe43e14a87bd7bb2e41ca545f1e
Dinocabbage <power0709@naver.com>
2023-04-11 14:55:41 +0900

    cabbage commit2

--- src/team1/main.java
+++ src/team1/main.java
@@ -4,10 +4,14 @@ public class main {
 
 	public static void main(String[] args) {
 	
-		Hobby hb = new Junwon();
+		Hobby hb = new Seorim();
+		Hobby hb2 = new Junwon();
+		Hobby juhyunHobby = new Juhyun();
 		Hobby junyeongHobby = new Junyeong();
 		
 		hb.print();
+		hb2.print();
+		juhyunHobby.print();
 		junyeongHobby.print();
 	}
 }
