63cd6aabeb12465af4b859b59126e19c4b8b7ef2
rudgusee <rudgusee@gmail.com>
2023-04-11 15:02:56 +0900

    rudgusee first commit

--- src/team1/main.java
+++ src/team1/main.java
@@ -5,8 +5,11 @@ public class main {
 	public static void main(String[] args) {
 	
 		Hobby hb = new Junwon();
+		Hobby rudgusee = new rudgusee();
 		
 		hb.print();
+		System.out.println();
+		rudgusee.print();
 	}
 
 }
